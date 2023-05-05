87e1ec1fedcdd72c8dc6daec2dc8430f84f045fa
Egor Dubovik <egorka54896@gmail.com>
2023-05-05 19:37:10 +0300

    feet: add funcions validateEmail and validatePassword

---
+++ src/helpers/validation.ts
@@ -0,0 +1,8 @@
+export const validateEmail = (email: string): boolean => {
+  const emailRegex = /^\S+@\S+\.\S+$/;
+  return emailRegex.test(email);
+};
+
+export const validatePassword = (password: string): boolean => {
+  return password.length >= 6;
+};
