bf9cbdd5078ec3ef5b422d9f40cae2f0e3beddb9
Egor Dubovik <egorka54896@gmail.com>
2023-05-05 20:00:13 +0300

    feet: add handling errors for LoginForm

--- src/components/auth/AuthButtons/AuthButtons.tsx
+++ src/components/auth/AuthButtons/AuthButtons.tsx
@@ -1,25 +1,35 @@
 import React, { FC } from 'react';
-import { Box, Button } from '@mui/material';
+import { Box } from '@mui/material';
 import { signInWithGoogle } from '../../../firebase/signIn';
 import useAuthButtonsStyles from './AuthButtons.style';
+import { LoadingButton } from '@mui/lab';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../../../firebase/config';
 
 const AuthButtons: FC = () => {
+  const [user, loading] = useAuthState(auth);
   const classes = useAuthButtonsStyles();
 
   return (
     <Box className={classes['login-form_buttons']}>
-      <Button className={classes['login-form_button']} type="submit" variant="contained">
+      <LoadingButton
+        loading={loading}
+        className={classes['login-form_button']}
+        type="submit"
+        variant="contained"
+      >
         Login
-      </Button>
-      <Button
+      </LoadingButton>
+      <LoadingButton
         className={classes['login-form_button']}
         onClick={signInWithGoogle}
         type="button"
+        loading={loading}
         variant="contained"
         color="secondary"
       >
         Login with Google
-      </Button>
+      </LoadingButton>
     </Box>
   );
 };
--- src/modules/forms/LoginForm/LoginForm.tsx
+++ src/modules/forms/LoginForm/LoginForm.tsx
@@ -1,16 +1,23 @@
 import React, { FC, FormEvent, useState, useEffect } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { Box, TextField, Typography } from '@mui/material';
+import { Box, FormHelperText, TextField, Typography } from '@mui/material';
 import { ROUTES } from '../../../router/routes/routes.constant';
 import AuthButtons from '../../../components/auth/AuthButtons/AuthButtons';
 import { logInWithEmailAndPassword } from '../../../firebase/logIn';
 import { auth } from '../../../firebase/config';
+import { validateEmail, validatePassword } from '../../../helpers/validation';
+
+interface IFormErrors {
+  email: string;
+  password: string;
+}
 
 const LoginForm: FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [user, loading, error] = useAuthState(auth);
+  const [errors, setErrors] = useState({} as IFormErrors);
+  const [user] = useAuthState(auth);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -18,33 +25,53 @@ const LoginForm: FC = () => {
     console.log(user);
   }, [user]);
 
+  const checkFielsd = (): void => {
+    const newErrors = {} as IFormErrors;
+    if (email && !validateEmail(email)) {
+      newErrors.email = 'Incorrect email';
+    }
+    if (password && !validatePassword(password)) {
+      newErrors.password = 'Password must contain at least 6 characters';
+    }
+    setErrors(newErrors);
+  };
+
+  useEffect(() => {
+    checkFielsd();
+  }, [email, password]);
+
   const handleLogIn = (event: FormEvent): void => {
     event.preventDefault();
-    logInWithEmailAndPassword(email, password);
+    setErrors({} as IFormErrors);
+    if (Object.keys(errors).length === 0) {
+      logInWithEmailAndPassword(email, password);
+      console.log(55);
+    }
   };
 
   return (
     <>
       {/* {error && <Typography color={'red'}>Error: {error.message}</Typography>} */}
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <Box className="login-form" onSubmit={handleLogIn} component="form">
+
+      <Box className="login-form" onSubmit={handleLogIn} component="form">
+        <Box sx={{ mb: '20px' }}>
           <TextField
-            error={false}
-            // helperText=""
+            error={!!errors.email}
             value={email}
             onChange={(event) => setEmail(event.target.value)}
-            sx={{ mb: '20px', display: 'block' }}
+            sx={{ display: 'block' }}
             fullWidth
             label="Email address"
             autoComplete="email"
             required
             autoFocus
           />
+          {errors.email && <FormHelperText error>{errors.email}</FormHelperText>}
+        </Box>
+
+        <Box sx={{ mb: '20px' }}>
           <TextField
-            error={false}
-            // helperText=""
+            error={!!errors.password}
             value={password}
             onChange={(event) => setPassword(event.target.value)}
             sx={{ mb: '20px', display: 'block' }}
@@ -53,16 +80,17 @@ const LoginForm: FC = () => {
             type="password"
             required
           />
-
-          <AuthButtons />
-          <Box>
-            <Typography>
-              Don&apos;t have an account? <NavLink to={ROUTES.REGISTRATION}>Register</NavLink> now.
-            </Typography>
-            <NavLink to={ROUTES.RESET}>Forgot password</NavLink>
-          </Box>
+          {errors.password && <FormHelperText error>{errors.password}</FormHelperText>}
         </Box>
-      )}
+
+        <AuthButtons />
+        <Box>
+          <Typography>
+            Don&apos;t have an account? <NavLink to={ROUTES.REGISTRATION}>Register</NavLink> now.
+          </Typography>
+          <NavLink to={ROUTES.RESET}>Forgot password</NavLink>
+        </Box>
+      </Box>
     </>
   );
 };
