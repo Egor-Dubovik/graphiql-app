c0603885dccaab67fa736369a9fdfa84e2cee45b
Egor Dubovik <egorka54896@gmail.com>
2023-05-08 20:15:47 +0300

    feet: add Loader and use it in App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { RouterProvider } from 'react-router-dom';
+import Loader from '../components/Loader/Loader';
+
+import { auth } from '../firebase/config';
 import router from '../router/router';
 import './App.scss';
 
 function App() {
+  const [user, isLoading] = useAuthState(auth);
+
   return (
-    <div className="App">
-      <RouterProvider router={router} />
-    </div>
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="App">
+          <RouterProvider router={router} />
+        </div>
+      )}
+    </>
   );
 }
 
