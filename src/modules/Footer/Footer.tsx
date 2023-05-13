990860bc528fedb318cf05144d33314aeb33f392
Harry177 <harry177@rambler.ru>
2023-05-13 04:36:59 +0200

    feat: add tooltip components for footer elements

--- src/modules/Footer/Footer.tsx
+++ src/modules/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { Container, ThemeProvider, Typography } from '@mui/material';
+import { Container, ThemeProvider, Tooltip, Typography } from '@mui/material';
 import React, { FC } from 'react';
 import {
   CenterFooterBox,
@@ -15,24 +15,30 @@ const Footer: FC = () => {
       <ThemeProvider theme={footerTheme}>
         <Container maxWidth="xl">
           <LeftFooterBox>
-            <a href="https://github.com/Egor-Dubovik">
-              <GhImage
-                src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
-                alt="github logo"
-              ></GhImage>
-            </a>
-            <a href="https://github.com/grishanova-oa">
-              <GhImage
-                src="https://www.svgrepo.com/show/365441/github-logo-thin.svg"
-                alt="github logo"
-              ></GhImage>
-            </a>
-            <a href="https://github.com/harry177">
-              <GhImage
-                src="https://cdn.cdnlogo.com/logos/g/55/github.svg"
-                alt="github logo"
-              ></GhImage>
-            </a>
+            <Tooltip title="E.Dubovik github">
+              <a href="https://github.com/Egor-Dubovik">
+                <GhImage
+                  src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
+                  alt="github logo"
+                ></GhImage>
+              </a>
+            </Tooltip>
+            <Tooltip title="O.Grishanova github">
+              <a href="https://github.com/grishanova-oa">
+                <GhImage
+                  src="https://www.svgrepo.com/show/365441/github-logo-thin.svg"
+                  alt="github logo"
+                ></GhImage>
+              </a>
+            </Tooltip>
+            <Tooltip title="A.Prygunov github">
+              <a href="https://github.com/harry177">
+                <GhImage
+                  src="https://cdn.cdnlogo.com/logos/g/55/github.svg"
+                  alt="github logo"
+                ></GhImage>
+              </a>
+            </Tooltip>
           </LeftFooterBox>
           <CenterFooterBox>
             <Typography
@@ -47,11 +53,13 @@ const Footer: FC = () => {
             </Typography>
           </CenterFooterBox>
           <RightFooterBox>
-            <InnerRSSBox>
-              <a href="https://rs.school/">
-                <img src="https://rs.school/images/rs_school.svg" alt="rss logo"></img>
-              </a>
-            </InnerRSSBox>
+            <Tooltip title="RSS web site">
+              <InnerRSSBox>
+                <a href="https://rs.school/">
+                  <img src="https://rs.school/images/rs_school.svg" alt="rss logo"></img>
+                </a>
+              </InnerRSSBox>
+            </Tooltip>
           </RightFooterBox>
         </Container>
       </ThemeProvider>
