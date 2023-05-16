48d74d527c65ccce1870d2316b696b9d0a64fbf4
Egor Dubovik <egorka54896@gmail.com>
2023-05-16 18:50:45 +0300

    refactor: add Breadcrumbs to SchemaPath instead string

--- src/components/schema/SchemaPath.tsx
+++ src/components/schema/SchemaPath.tsx
@@ -2,16 +2,17 @@ import React, { FC } from 'react';
 import Typography from '@mui/material/Typography';
 import { useAppSelector } from '../../app/store/hooks';
 import { selectSchemaStack } from '../../features/Sheme/schemaSlice';
+import { Breadcrumbs } from '@mui/material';
 
 const SchemaPath: FC = () => {
   const { path } = useAppSelector(selectSchemaStack);
 
   return (
-    <Typography sx={{ p: '10px 16px' }}>
-      {path.reduce((acc, pathItem) => {
-        return (acc += `${pathItem}/`);
-      }, '')}
-    </Typography>
+    <Breadcrumbs separator="›" sx={{ p: '10px 16px' }}>
+      {path.map((pathItem) => (
+        <Typography key={pathItem}>{pathItem}</Typography>
+      ))}
+    </Breadcrumbs>
   );
 };
 
