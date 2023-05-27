d069339b7e42d95ca118a1acaa8b5a8369a0f19f
Egor Dubovik <egorka54896@gmail.com>
2023-05-27 17:35:17 +0300

    add cofiguratin for production build

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,12 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: '',
+    sourcemap: false,
+    minify: true,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
